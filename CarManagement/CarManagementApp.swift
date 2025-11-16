e623afb7376e27a2adbed1840d57d034be86e075
Cinix03 <vasileirinel013@gmail.com>
2025-11-16 15:38:55 +0200

    Initial Commit

---
+++ CarManagement/CarManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarManagementApp.swift
+//  CarManagement
+//
+//  Created by George Vasile on 16.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct CarManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
